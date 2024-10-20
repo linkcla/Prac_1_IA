0fa40265d95082e2ada913c9ec5f10706b646230
Marc Link <linkcla10@gmail.com>
2024-10-20 10:42:21 +0200

    [Added practice statement]

---
+++ practica/agent.py
@@ -0,0 +1,33 @@
+import random
+
+from practica import joc
+from practica.joc import Accions
+
+
+class Viatger(joc.Viatger):
+    def __init__(self, *args, **kwargs):
+        super(Viatger, self).__init__(*args, **kwargs)
+        self.__proves = [
+            (Accions.MOURE, "E"),
+            (Accions.MOURE, "S"),
+            (Accions.MOURE, "N"),
+            (Accions.MOURE, "O"),
+            (Accions.BOTAR, "S"),
+            (Accions.BOTAR, "N"),
+            (Accions.BOTAR, "E"),
+            (Accions.BOTAR, "O"),
+            (Accions.POSAR_PARET, "S"),
+            (Accions.POSAR_PARET, "N"),
+            (Accions.POSAR_PARET, "E"),
+            (Accions.POSAR_PARET, "O"),
+
+        ]
+
+    def pinta(self, display):
+        pass
+
+    def actua(self, percepcio: dict) -> Accions | tuple[Accions, str]:
+        if self.__proves:
+            acc = random.choice(self.__proves)
+            return acc
+        return Accions.ESPERAR
